4d509e08dca77ebb2b5cbcf9d71adc1246bab903
Paulo Henrique Araújo <pauloaraujoDEV25@gmail.com>
2026-02-06 01:56:45 -0300

    .
    .

--- src/main/java/br/com/mktech/controller/TestController.java
+++ src/main/java/br/com/mktech/controller/TestController.java
@@ -1,10 +1,15 @@
 package br.com.mktech.controller;
 
+import br.com.mktech.service.IPackageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    
+    @Autowired(required = false)
+    private IPackageService packageService;
     
     @GetMapping("/test")
     public String test() {
@@ -14,5 +19,17 @@ public class TestController {
     @GetMapping("/health")
     public String health() {
         return "OK";
+    }
+    
+    @GetMapping("/api/packages")
+    public String packages() {
+        try {
+            if (packageService == null) {
+                return "PackageService é null!";
+            }
+            return "Pacotes: " + packageService.getAllPackages().size();
+        } catch (Exception e) {
+            return "Erro: " + e.getMessage() + " - " + e.getClass().getName();
+        }
     }
 }
