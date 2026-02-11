8cd723d8367ba422fb74841146b70aa9c37d6952
Paulo Henrique Araújo <pauloaraujoDEV25@gmail.com>
2026-02-10 21:03:49 -0300

    .
    .

--- src/main/java/br/com/mktech/config/WebConfig.java
+++ src/main/java/br/com/mktech/config/WebConfig.java
@@ -7,32 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * Configuração Web para servir arquivos estáticos.
  *
- * Garante que CSS, JS e imagens sejam servidas corretamente
- * sem depender do Spring Security.
+ * Garante que CSS, JS e imagens sejam servidas corretamente.
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Servir arquivos CSS
-        registry.addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/css/")
-                .setCachePeriod(3600);
-
-        // Servir arquivos JavaScript
-        registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/static/js/")
-                .setCachePeriod(3600);
-
-        // Servir arquivos de imagem
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/")
-                .setCachePeriod(3600);
-
-        // Servir outros arquivos estáticos
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/")
-                .setCachePeriod(3600);
+        // Servir todos os arquivos estáticos sem cache
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
